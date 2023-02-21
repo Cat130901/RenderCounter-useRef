bc45485eeee5230f23c7bccdc3ba3b1e33b394da
Cat130901 <124379760+Cheongwenghong@users.noreply.github.com>
2023-02-21 15:51:33 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
+import {useState, useRef, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState('')
+  const count = useRef(0)
+
+  useEffect(() => {
+    count.current += 1
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={inputValue}
+        onChange={(event)=>setInputValue(event.target.value)}
+        type="text"
+      />
+      <h1>counting {count.current}</h1>
     </div>
   );
 }
